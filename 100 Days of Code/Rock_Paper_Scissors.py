91377e918b1e401bf2bc9dea1a1b544625009f33
Jerry Vander Weide <59615980+JerryVW@users.noreply.github.com>
2026-01-14 02:10:32 -0500

    Initial commit

---
+++ 100 Days of Code/Rock_Paper_Scissors.py
@@ -0,0 +1,99 @@
+import random
+
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+paper = """
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+random_choice = random.randint(0, 2)
+computer_choice = [rock, paper, scissors]
+player_choice = [rock, paper, scissors]
+player_input = int(
+    input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors: ")
+)
+# print(f"{player_choice[player_input]} vs {computer_choice[random_choice]}")
+if player_input >= 0 and player_input <= 2:
+    if (
+        player_choice[player_input] == rock
+        and computer_choice[random_choice] == scissors
+    ):
+        print("Player chose:")
+        print(player_choice[player_input])
+        print("computer chose:")
+        print(computer_choice[random_choice])
+        print("Rock beats scissors! You win!")
+    elif (
+        player_choice[player_input] == scissors
+        and computer_choice[random_choice] == paper
+    ):
+        print("Player chose:")
+        print(player_choice[player_input])
+        print("computer chose:")
+        print(computer_choice[random_choice])
+        print("Scissors beats paper! You win!")
+    elif (
+        player_choice[player_input] == paper and computer_choice[random_choice] == rock
+    ):
+        print("Player chose:")
+        print(player_choice[player_input])
+        print("computer chose:")
+        print(computer_choice[random_choice])
+        print("Paper beats rock! You win!")
+
+    elif (
+        computer_choice[random_choice] == rock
+        and player_choice[player_input] == scissors
+    ):
+        print("Player chose:")
+        print(player_choice[player_input])
+        print("computer chose:")
+        print(computer_choice[random_choice])
+        print("Rock beats scissors! You lose!")
+    elif (
+        computer_choice[random_choice] == scissors
+        and player_choice[player_input] == paper
+    ):
+        print("Player chose:")
+        print(player_choice[player_input])
+        print("computer chose:")
+        print(computer_choice[random_choice])
+        print("Scissors beats paper! You lose!")
+    elif (
+        computer_choice[random_choice] == paper and player_choice[player_input] == rock
+    ):
+        print("Player chose:")
+        print(player_choice[player_input])
+        print("computer chose:")
+        print(computer_choice[random_choice])
+        print("Paper beats rock! You lose!")
+
+    elif player_choice[player_input] == computer_choice[random_choice]:
+        print("Player chose:")
+        print(player_choice[player_input])
+        print("computer chose:")
+        print(computer_choice[random_choice])
+        print("It's a Draw!")
+else:
+    print("Not a valid input!")
---
+++ 100 Days of Code/Treasure_Island.py
@@ -0,0 +1,61 @@
+print(
+    r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+'''
+)
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+direction = (
+    input("You're at a cross road. Where do you want to go?" "Type 'left' or 'right'.")
+).lower()
+if direction == "left":
+    print("You've come to a lake. There is an island in the middle of the lake")
+    to_the_island = input(
+        "Type 'wait' tp wait for the boat. Type 'swim' to swim across."
+    ).lower()
+    if to_the_island == "wait":
+        print("You arrive at the island unharmed. There is a house with three doors")
+        choice_of_door = input(
+            "One red, one yellow, and on blue. Which door do you choose?"
+        ).lower()
+        if choice_of_door == "red":
+            print("You were burned to death by fire!")
+            print("GAME OVER!")
+        elif choice_of_door == "yellow":
+            print("Bingo!! You have found the treasure!!")
+            print("YOU WIN!!")
+        elif choice_of_door == "blue":
+            print("You were eaten by the great beast!")
+            print("GAME OVER!")
+        else:
+            print("There is no other way!!")
+    elif to_the_island == "swim":
+        print("You were swallowed by the Lochness Monster!!")
+        print("GAME OVER!")
+    else:
+        print("There is no way in that manner to cross the lake.")
+
+elif direction == "right":
+    print("Oh No! You fell in a hole!")
+    print("GAME OVER!")
+else:
+    print("That direction is not an option!")
