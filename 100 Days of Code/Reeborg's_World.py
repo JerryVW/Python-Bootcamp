e51df59a710b4c6b437fc1ff56a77efd09169e66
Jerry Vander Weide <59615980+JerryVW@users.noreply.github.com>
2026-01-17 11:44:10 -0500

    Reeborg's World
    Code for maze on Reeborg's world online app.

---
+++ 100 Days of Code/Reeborg's_World.py
@@ -0,0 +1,20 @@
+# Only works in Reeborg's World app online
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# while front_is_clear():
+#     move()
+# turn_left()
+
+# while not_at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
